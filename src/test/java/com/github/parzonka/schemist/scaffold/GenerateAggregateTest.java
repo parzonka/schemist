edb7b874c7e1ea81cf4fded53153e57d0af74bfc
parzonka <parzonka@gmail.com>
2020-04-08 17:19:42 +0200

    extract method

--- src/test/java/com/github/parzonka/schemist/scaffold/GenerateAggregateTest.java
+++ src/test/java/com/github/parzonka/schemist/scaffold/GenerateAggregateTest.java
@@ -1,5 +1,6 @@
 package com.github.parzonka.schemist.scaffold;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,20 +15,23 @@ public class GenerateAggregateTest {
 
   @Test
   public void should_generate() throws Exception {
-    final AggregateSpec a = AggregateSpec.builder()
+    final AggregateSpec aggregateSpec = AggregateSpec.builder()
         .aggregateType(Person.class)
         .localizedSingular("Person")
         .localizedPlural("Persons")
         .schemaUrl("/schema/person.yaml")
         .build();
+    writeAggregate(aggregateSpec, "target/generated/");
+    return;
+  }
+
+  private void writeAggregate(final AggregateSpec a, String prefix) throws IOException {
     final String aggregate = GenerateAggregate.aggregate(a, getClass().getPackageName());
     final String dir = "target/generated/src/test/java/" + getClass().getPackageName()
         .replace(".", "/");
     Path dirPath = Paths.get(dir);
     Path filePath = Paths.get(dir + "/" + a.getLocalizedSingular() + "Aggregate.java");
-    System.out.println(dirPath);
     Files.createDirectories(dirPath);
-    System.out.println(aggregate);
     Files.writeString(filePath, aggregate, Charset.forName("UTF-8"));
   }
 
